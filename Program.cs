eca8fd3deeae3c6ac875461573bd17b5a15a730e
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-07 21:46:07 -0300

    test

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Caracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+
+            string nombre, apellido;
+
+            Console.WriteLine("Ingrese Nombre");
+            
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese Apellido");
+            apellido = Console.ReadLine();
+            
+           
+            
+            Console.WriteLine("Su Nombre Compelto es: " + nombre + apellido);
+
+            Console.ReadLine();
+
+        }
+    }
+}
